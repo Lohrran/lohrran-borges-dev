959dae11600cceeb83f9bcaf84ff3c504e13f8e9
Lohrran Borges Farias <lborgesf@everis.com>
2020-06-29 22:55:40 -0500

    *conf.js

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 window.appConf = {
     author: 'Lohrran Borges',
-    dashboardId: '1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4',
+    dashboardId: '1zK0SPTzPLgXQqBNKf_M13WrKXvBM-rYDogsr1XW0mtY',
     sendContactMessageUrlId:
         'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
     shortname: 'easydrivecms',
